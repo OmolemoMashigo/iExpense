c1b489d5c3d382796146e884782e3125a91ba29d
Omolemo Mashigo <omomashigo@gmail.com>
2025-03-05 19:11:36 +0200

    added AddView and shared observed Expenses object with new view

---
+++ AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Omolemo Mashigo on 2025/03/05.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
